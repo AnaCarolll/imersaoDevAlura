f433ffe97fbb31f11ca34338bdb3db42ec47120d
Ana Carol <progamacaocarol2021@gmail.com>
2023-11-16 17:18:18 -0300

    Criado o projeto metalista

---
+++ mentalista/index.js
@@ -0,0 +1,14 @@
+var numeroSecreto = parseInt(Math.random()*1001);
+while (chute != numeroSecreto) {
+    var chute = prompt('Digite um numero entre 0 e 1000: ')
+    if(chute == numeroSecreto){
+        alert("você acertou!")
+    }else if (chute > numeroSecreto){
+       alert("o numero secreto é menor que o numero fornecido!")
+       
+    } else if(chute < numeroSecreto){
+        alert("o numero secreto é maior que o numero fornecido!")
+    }
+}
+
+
