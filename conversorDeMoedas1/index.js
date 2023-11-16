5ed3bbf96175c321ceb57e70eeb9d3527b6eefaf
Ana Carol <progamacaocarol2021@gmail.com>
2023-11-16 16:34:40 -0300

    Adicionei novas conversões

--- conversorDeMoedas1/index.js
+++ conversorDeMoedas1/index.js
@@ -1,1 +1,34 @@
-alert("Ola tudo bem ?")
+//CONVERSOR EM DOLAR
+// var valorEmDolar = 30;
+// var cotacaoDoDolar = 5.32;
+// var realEmDolar =  valorEmDolar*cotacaoDoDolar;
+// realEmDolar = realEmDolar.toFixed(2)
+// alert( "R$" + realEmDolar)
+
+
+//COVENSOR EM LIBRA 
+// var valorEmLibra = 9
+// var cotacaoEmReal = 6.04
+// var libraEmReal = cotacaoEmReal * valorEmLibra
+// alert(libraEmReal)
+
+
+//CONVERSOR EM EURO 
+// var valorEmEuro = 7
+// var cotacaoEmReal =  5.28
+// var euroEmReal = valorEmEuro * cotacaoEmReal
+// alert(euroEmReal)
+
+
+//GRAU CELSIUS PARA KELVIN
+// var grauCelsius = 2
+// var grauKelvin = 273.15
+// var celsiusEmKelvin = grauCelsius + grauKelvin
+// alert(celsiusEmKelvin)
+
+
+//GRAU CELSIUS PARA FAHRENHEIT
+// var grauCelsius = 2;
+// var grauFixoFahrenheit = 32;
+// const formula =  (grauCelsius*9/5) + grauFixoFahrenheit
+// alert (formula)
